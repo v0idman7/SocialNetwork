7d2ed3573e980952b9a5e54a3c641a02c4795a47
Sergey <sergeyvoitkevich@mail.ru>
2022-01-11 14:16:42 +0300

    Add MainNavigation component

--- Client/src/components/App/App.tsx
+++ Client/src/components/App/App.tsx
@@ -1,5 +1,11 @@
+import { Route, Routes } from 'react-router-dom';
+import MainNavigation from '../MainNavigation/MainNavigation';
 import './App.scss';
 
-const App = () => <div />;
+const App = () => (
+  <Routes>
+    <Route path='/' element={<MainNavigation />} />
+  </Routes>
+);
 
 export default App;
